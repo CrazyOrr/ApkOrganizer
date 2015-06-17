823d492963608d1a1727b2a8057ed5260a486800
wanglei <wanglei021212@gmail.com>
2015-06-17 13:54:18 +0800

    support both Eclipse and Android Studio projects

--- apk_organizer.py
+++ apk_organizer.py
@@ -10,6 +10,7 @@ import os
 import datetime
 import shutil
 import locale
+from distutils.dir_util import copy_tree
 
 CHARSET_UTF8 = 'utf-8'
 CHARSET_CURRENT = locale.getpreferredencoding()
@@ -36,6 +37,16 @@ def list_files(d, file_filter=None, ignore=()):
             if file_filter is None or file_filter(x):
                 yield full_dir
 
+def find_dir(d, name):
+    for dirpath, dirnames, filenames in os.walk(d):
+        if name in dirnames:
+            return os.path.join(dirpath, name)
+        
+def find_file(d, name):
+    for dirpath, dirnames, filenames in os.walk(d):
+        if name in filenames:
+            return os.path.join(dirpath, name)
+
 def read_config():
     config_dict = {}
     config_file_path = os.path.join(os.path.curdir, 'config.txt')
@@ -53,6 +64,10 @@ def read_config():
     print 'To: %s' % destination_path
     return android_project_path, destination_path
 
+def is_release_apk(n):
+    n_arr = os.path.splitext(n)
+    return n_arr[1] == '.apk' and not n_arr[0].endswith('debug') and not n_arr[0].endswith('unaligned')
+
 def copy_apk_files(src, dst):
     print 'Start'
     # current datetime to be the name of the folder
@@ -61,18 +76,26 @@ def copy_apk_files(src, dst):
     make_dir(dst_base)
     src_base = convert_encoding(src, CHARSET_UTF8, CHARSET_CURRENT)
     
-    for src_apk in list_files(src_base, lambda n: os.path.splitext(n)[1] == '.apk', ('bin',)):
+    for src_apk in list_files(src_base, is_release_apk, ('bin',)):
         apk_name = os.path.split(src_apk)[1]
         dst_apk = os.path.join(dst_base, apk_name)
         shutil.copyfile(src_apk, dst_apk)
         print 'Copy %s to %s' % (convert_encoding(src_apk, CHARSET_CURRENT, CHARSET_UTF8),
                              convert_encoding(dst_apk, CHARSET_CURRENT, CHARSET_UTF8))
     
-    mapping_name = 'mapping.txt'
-    src_mapping = os.path.join(src_base, 'proguard', mapping_name)
-    dst_mapping = os.path.join(dst_base, mapping_name)
-    shutil.copyfile(src_mapping, dst_mapping)
-    print 'Copy %s to %s' % (convert_encoding(src_mapping, CHARSET_CURRENT, CHARSET_UTF8),
+    # 目录中包含build.gradle文件的为Android Studio的project，否则为Eclipse的project
+    if find_file(src_base, 'build.gradle'):
+        # Android Studio
+        mapping_dirname = 'mapping'
+    else:
+        # Eclipse
+        mapping_dirname = 'proguard'
+    
+    src_mapping = find_dir(src_base, mapping_dirname)
+    if src_mapping:
+        dst_mapping = os.path.join(dst_base, mapping_dirname)
+        copy_tree(src_mapping, dst_mapping)
+        print 'Copy %s into %s' % (convert_encoding(src_mapping, CHARSET_CURRENT, CHARSET_UTF8),
                              convert_encoding(dst_mapping, CHARSET_CURRENT, CHARSET_UTF8))
     
     print 'Done'
@@ -80,4 +103,5 @@ def copy_apk_files(src, dst):
 if __name__ == '__main__':
     src, dst = read_config()
     copy_apk_files(src, dst)
+
     
